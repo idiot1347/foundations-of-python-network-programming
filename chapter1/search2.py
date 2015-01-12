1e0f75d16c3f8123ddf66ccd53f78754482e2748
Zekui Shi <idiot1347@gmail.com>
2015-01-12 19:56:53 +0800

    chapter1 search2g

---
+++ chapter1/search2.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def geocode(address):
+    parameters = {'address': address, 'sensor': 'false'}
+    base = 'http://maps.googleapis.com/maps/api/geocode/json'
+    response = requests.get(base, params=parameters)
+    answer = response.json()
+    print(answer['results'][0]['geometry']['location'])
+
+if __name__ == '__main__':
+    geocode('207 N. Defiance St, Archbold, OH')
